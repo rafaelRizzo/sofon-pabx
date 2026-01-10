22f82bfa672e6402ca5ac5de8e204eec38f0c81d
Rafael Rizzo <92104771+rafaelRizzo@users.noreply.github.com>
2026-01-10 17:33:46 -0300

    chore: alterado autorização das rotas de users
    - Somente admin consegue utilizar os recursos protegidos.

--- backend/src/routes/user/user.route.ts
+++ backend/src/routes/user/user.route.ts
@@ -1,15 +1,15 @@
 import { type FastifyInstance } from 'fastify'
-import { authMiddleware } from '../../middlewares/middleware.auth'
+import { adminMiddleware } from '../../middlewares/middleware.auth'
 import { createUserSchema, updateUserSchema, getUserSchema, deleteUserSchema } from './schema/user.schema'
 import { createUser, deleteUser, firstUser, getUser, listUsers, updateUser } from '../../controllers/user/user.controller'
 
 export const userRoutes = async (fastify: FastifyInstance) => {
-    const auth = { preHandler: authMiddleware }
+    const authAdmin = { preHandler: adminMiddleware }
 
     fastify.post('/first-user', { schema: createUserSchema }, firstUser)
-    fastify.post('/users', { ...auth, schema: createUserSchema }, createUser)
-    fastify.get('/users', auth, listUsers)
-    fastify.get('/users/:id', { ...auth, schema: getUserSchema }, getUser)
-    fastify.put('/users/:id', { ...auth, schema: updateUserSchema }, updateUser)
-    fastify.delete('/users/:id', { ...auth, schema: deleteUserSchema }, deleteUser)
+    fastify.post('/users', { ...authAdmin, schema: createUserSchema }, createUser)
+    fastify.get('/users', authAdmin, listUsers)
+    fastify.get('/users/:id', { ...authAdmin, schema: getUserSchema }, getUser)
+    fastify.put('/users/:id', { ...authAdmin, schema: updateUserSchema }, updateUser)
+    fastify.delete('/users/:id', { ...authAdmin, schema: deleteUserSchema }, deleteUser)
 }
