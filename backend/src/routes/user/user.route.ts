72d935831520dbdb88ce89467d564ad7d4f93861
Rafael Rizzo <92104771+rafaelRizzo@users.noreply.github.com>
2026-01-10 17:54:11 -0300

    fix: ajuste no middleware

--- backend/src/routes/user/user.route.ts
+++ backend/src/routes/user/user.route.ts
@@ -1,10 +1,10 @@
 import { type FastifyInstance } from 'fastify'
-import { adminMiddleware } from '../../middlewares/middleware.auth'
+import { authMiddleware, adminMiddleware } from '../../middlewares/middleware.auth'
 import { createUserSchema, updateUserSchema, getUserSchema, deleteUserSchema } from './schema/user.schema'
 import { createUser, deleteUser, firstUser, getUser, listUsers, updateUser } from '../../controllers/user/user.controller'
 
 export const userRoutes = async (fastify: FastifyInstance) => {
-    const authAdmin = { preHandler: adminMiddleware }
+    const authAdmin = { preHandler: authMiddleware, adminMiddleware }
 
     fastify.post('/first-user', { schema: createUserSchema }, firstUser)
     fastify.post('/users', { ...authAdmin, schema: createUserSchema }, createUser)
